2d8db56e3e3782cb73b92c36ab0aef6fd843c859
Renan <86449025+iRnx@users.noreply.github.com>
2022-02-15 22:09:16 -0300

    Add files via upload

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,1 +1,13 @@
+$("input[autocomplete='off']").each(function () {
 
+    var input = $(this);
+    var id = $(this).attr("id");
+    var name = $(this).attr("name");
+
+    input.removeAttr("name").removeAttr("id");
+
+    setTimeout(function () {
+        input.attr("name", name).attr("id", id);
+    }, 1);
+
+});
