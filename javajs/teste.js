955c0ced1372e3bf6b27a5290e312ab8fe7e1374
Renan <86449025+iRnx@users.noreply.github.com>
2022-02-15 23:26:48 -0300

    Delete javajs directory

--- javajs/teste.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function(){
-  document.getElementById('nomesobrenome').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('telefone').value = '';
-},420);
