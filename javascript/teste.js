a84ede13e12d7b2309ee88726500fa5acb285185
Renan <86449025+iRnx@users.noreply.github.com>
2022-02-15 22:33:15 -0300

    Update teste.js

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,13 +1,2 @@
-$("input[autocomplete='off']").each(function () {
+input.setAttribute('autocomplete','off');
 
-    var input = $(this);
-    var id = $(this).attr("id");
-    var name = $(this).attr("name");
-
-    input.removeAttr("name").removeAttr("id");
-
-    setTimeout(function () {
-        input.attr("name", name).attr("id", id);
-    }, 1);
-
-});
