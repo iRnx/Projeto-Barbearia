c0d446ecd582866a080b9204065d70f28f0296bf
Renan <86449025+iRnx@users.noreply.github.com>
2022-02-15 23:17:08 -0300

    Add files via upload

--- javajs/teste.js
+++ javajs/teste.js
@@ -1,1 +1,9 @@
-
+// Esperar o documento carregar
+$(document).ready(function () {
+    // Usando um pequeno delay de 100ms porque às vezes o navegador preenche o campo logo que o documento está pronto, e pode não funcionar como esperado
+    setTimeout(function(){
+      $('#email').removeAttr('disabled');
+      $('#nomesobrenome').removeAttr('disable');
+      $('#telefone').removeAttr('disable');
+    }, 100);
+ });
